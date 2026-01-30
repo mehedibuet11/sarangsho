d28b5b2f66e6836c4328c1e8e1ed4416e0c91707
mehedibuet11 <mehedibuet11@gmail.com>
2026-01-30 14:24:14 +0600

    refactor: update pm2 configuration to use standalone server script

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "Sarangsho",
-      script: "pnpm",
-      args: "start",
+      script: ".next/standalone/server.js",
+      cwd: "/home/sarangsho/htdocs/sarangsho.com",
       instances: 1,
       exec_mode: "cluster",
       listen_timeout: 10000,
